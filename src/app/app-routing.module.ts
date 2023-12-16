9ce77edcb18ef05b4f2bbeed4df459af4ef26e8a
Douglas Silva <douglas.silva@orionconsult.com.br>
2023-12-16 16:42:37 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: "home", 
+    loadChildren: ()=> import('./home/home.module').then(m => m.HomeModule)
+  },
+  { 
+    path: "usuario", 
+    loadChildren: ()=> import('./usuario/usuario.module').then(m => m.UsuarioModule)
+  },
+  { 
+    path: "", 
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
